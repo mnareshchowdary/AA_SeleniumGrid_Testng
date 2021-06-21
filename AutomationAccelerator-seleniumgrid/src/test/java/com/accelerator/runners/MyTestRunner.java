aef5d26dcd53ccd5019b8518bcf2897d95293110
Naresh Test <mnareshchowdary@gmail.com>
2021-06-21 12:48:38 +0530

    Initial commit

--- AutomationAccelerator-seleniumgrid/src/test/java/com/accelerator/runners/MyTestRunner.java
+++ AutomationAccelerator-seleniumgrid/src/test/java/com/accelerator/runners/MyTestRunner.java
@@ -1,11 +1,18 @@
 package com.accelerator.runners;
 
-import org.junit.runner.RunWith;
+//import org.junit.runner.RunWith;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
+import io.cucumber.testng.TestNGCucumberRunner;
+import io.cucumber.testng.CucumberOptions;
 
-@RunWith(Cucumber.class)
+import org.testng.annotations.DataProvider;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+
+//import io.cucumber.junit.Cucumber;
+//import io.cucumber.junit.CucumberOptions;
+
+
 @CucumberOptions(
         features = {"src/test/resources/features/"},
         glue = {"com.accelerator.stepdefinitions", "com.accelerator.hooks"},
@@ -17,6 +24,12 @@ import io.cucumber.junit.CucumberOptions;
         dryRun= false
 )
 
-public class MyTestRunner {
+public class MyTestRunner extends AbstractTestNGCucumberTests {
+	
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
 
 }
--- AutomationAccelerator-seleniumgrid/test-output-thread/report.js
+++ AutomationAccelerator-seleniumgrid/test-output-thread/report.js
@@ -1,69 +1,81 @@
 $(document).ready(function() {
 CucumberHTML.timelineItems.pushArray([
   {
-    "id": "4bb15c2a-3ce0-48ba-ad19-17062a6db3f7",
-    "feature": "Account Page Feature",
-    "scenario": "Accounts section count",
-    "start": 1624208952524,
-    "group": 17,
-    "content": "",
-    "tags": "@accounts,",
-    "end": 1624208972811,
-    "className": "passed"
-  },
-  {
-    "id": "254de93e-d08b-40c8-bddc-ea1d29e195b0",
+    "id": "c79d2799-892b-46d4-9596-4caf58b49355",
     "feature": "Account Page Feature",
     "scenario": "Accounts page title",
-    "start": 1624208931285,
+    "start": 1624257611625,
     "group": 17,
     "content": "",
     "tags": "@accounts,",
-    "end": 1624208952505,
+    "end": 1624257643019,
     "className": "passed"
   },
   {
-    "id": "2b16492f-ead9-4bd8-901f-1f849d90d182",
-    "feature": "Login page feature",
-    "scenario": "Login page title",
-    "start": 1624208931290,
+    "id": "abc8ba31-06fc-4dba-a350-976cb6a5ef1e",
+    "feature": "Account Page Feature",
+    "scenario": "Accounts section count",
+    "start": 1624257611631,
     "group": 18,
     "content": "",
-    "tags": "",
-    "end": 1624208946132,
+    "tags": "@accounts,",
+    "end": 1624257630950,
     "className": "passed"
   },
   {
-    "id": "3e45b9f4-4c66-4d68-98f9-0ffd38889f2c",
-    "feature": "Login page feature",
-    "scenario": "Forgot Password link",
-    "start": 1624208946157,
-    "group": 18,
-    "content": "",
-    "tags": "",
-    "end": 1624208957483,
-    "className": "passed"
-  },
-  {
-    "id": "9bb0e805-6579-4c79-837e-8f13ac8ed9ce",
+    "id": "f95a5a8a-f0f9-445e-8ea5-3bd7dab1c48a",
     "feature": "Login page feature",
     "scenario": "Login with correct credentials",
-    "start": 1624208957500,
-    "group": 18,
+    "start": 1624257611628,
+    "group": 21,
     "content": "",
     "tags": "",
-    "end": 1624208972588,
+    "end": 1624257646531,
+    "className": "passed"
+  },
+  {
+    "id": "9cc3721c-a038-48bd-bc55-03d4bcad0545",
+    "feature": "Login page feature",
+    "scenario": "Login page title",
+    "start": 1624257611632,
+    "group": 19,
+    "content": "",
+    "tags": "",
+    "end": 1624257633205,
+    "className": "passed"
+  },
+  {
+    "id": "a35a95b9-9019-4588-87f3-29ed328a718e",
+    "feature": "Login page feature",
+    "scenario": "Forgot Password link",
+    "start": 1624257611544,
+    "group": 20,
+    "content": "",
+    "tags": "",
+    "end": 1624257624789,
     "className": "passed"
   }
 ]);
 CucumberHTML.timelineGroups.pushArray([
   {
     "id": 17,
-    "content": "Thread[pool-1-thread-1,5,main]"
+    "content": "Thread[TestNG-PoolService-0,5,main]"
   },
   {
     "id": 18,
-    "content": "Thread[pool-1-thread-2,5,main]"
+    "content": "Thread[TestNG-PoolService-1,5,main]"
+  },
+  {
+    "id": 19,
+    "content": "Thread[TestNG-PoolService-2,5,main]"
+  },
+  {
+    "id": 20,
+    "content": "Thread[TestNG-PoolService-3,5,main]"
+  },
+  {
+    "id": 21,
+    "content": "Thread[TestNG-PoolService-4,5,main]"
   }
 ]);
 });
